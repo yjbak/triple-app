0f4e6eddd38f863c1f32c2ef4c2c715e7eb1b855
yjpark <yjpark@wemakeprice.com>
2022-07-05 14:49:30 +0900

    feat: 린트 수정

--- src/components/ContentLogo/index.tsx
+++ src/components/ContentLogo/index.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const ContentLogo = styled.div`
-  background-image: url('triple2x.png');
+  background-image: url("triple2x.png");
   background-repeat: no-repeat;
   position: relative;
   width: 400px;
@@ -15,5 +15,5 @@ const ContentLogo = styled.div`
   > span {
     padding-bottom: 40px;
   }
-`
+`;
 export default ContentLogo;
